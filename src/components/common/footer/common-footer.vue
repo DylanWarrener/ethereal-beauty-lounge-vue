3c74a7f0577bf8a042d63cf46dafd73d1c916380
DylanWarrener <dqyqlqaqn@gmail.com>
2024-02-21 07:38:34 +0000

    init

--- src/components/common/footer/common-footer.vue
+++ src/components/common/footer/common-footer.vue
@@ -1,12 +1,32 @@
 <template>
-	<v-footer class="d-flex flex-column bg-accent" style="border: 2px solid black">
-		<v-container fluid style="border: 2px solid red">
-			<v-row dense style="border: 2px solid green">
-				<v-col style="border: 2px solid blue">
-					<p>Get connected with us on social networks!</p>
+	<v-footer class="d-flex flex-column bg-accent">
+		<v-container fluid>
+			<v-row dense>
+				<!-- Socials -->
+				<v-col cols="12" style="border: 2px solid black">
+					<v-row dense style="border: 2px solid orange">
+						<v-col cols="12" md="6" class="d-flex justify-center align-center" style="border: 2px solid blue">
+							<p>Get connected with us on social networks!</p>
+						</v-col>
+						<v-col cols="12" md="6" class="footer_socials d-flex flex-row justify-space-evenly align-center" style="border: 2px solid yellow">
+							<v-btn icon :key="index" v-for="(svg, index) in socials">
+								<v-icon size="x-large">
+									<template #default>
+										<v-img :src="svg"></v-img>
+									</template>
+								</v-icon>
+							</v-btn>
+						</v-col>
+					</v-row>
 				</v-col>
-				<v-col class="d-flex flex-row justify-space-evenly align-center	" style="border: 2px solid yellow">
-					<v-btn class="footer_socials_icon" :icon="icon_facebook" color="transparent"></v-btn>
+
+				<!-- Middle -->
+				<v-col cols="12" style="border: 2px solid black">
+					<v-row dense style="border: 2px solid orange">
+						<v-col cols="12" class="d-flex flex-row justify-space-evenly align-center" style="border: 2px solid blue">
+							<v-btn variant="flat" class="bg-transparent" :key="index" v-for="(item, index) in navigation">{{ item }}</v-btn>
+						</v-col>
+					</v-row>
 				</v-col>
 			</v-row>
 		</v-container>
@@ -16,9 +36,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-// Components
-import useFooterStore from '@stores/store-footer.js';
-
 // SVGs
 import FacebookSVG from "@assets/svg/socials/facebook.svg";
 import InstagramSVG from "@assets/svg/socials/instagram.svg";
@@ -26,34 +43,35 @@ import TikTokSVG from "@assets/svg/socials/tiktok.svg";
 
 export default defineComponent({
 	name: 'footer-component',
-	computed: {
-		/* Icons */
-		icon_facebook(): string {
-			return FacebookSVG;
-		},
-		icon_instagram(): string {
-			return InstagramSVG;
-		},
-		icon_tiktok(): string {
-			return TikTokSVG;
+	data() {
+		return {
+			socials: [
+				FacebookSVG,
+				InstagramSVG,
+				TikTokSVG
+			],
+			navigation: [
+				"Home",
+				"Treatments",
+				"Store",
+				"Contact",
+				"Reviews",
+				"About"
+			]
 		}
-	},
-	setup() {
-		const storeHeader = useFooterStore();
-		return { storeHeader };
-	},
+	}
 });
 </script>
 
 <style lang="scss">
-.footer_socials_icon {
+.footer_socials {
 	.v-btn {
 		.v-btn__content {
 			.v-icon {
-				.v-icon__svg {
-					//color: rgb(var(--v-theme-inverted));
-					fill: rgb(var(--v-theme-default));
-					//background: rgb(var(--v-theme-accent));
+				.v-img {
+					.v-img__img {
+						filter: invert(52%) sepia(44%) saturate(320%) hue-rotate(285deg) brightness(94%) contrast(95%);
+					}
 				}
 			}
 		}
