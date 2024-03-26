d15cf396839bb87be4137846cec2bd62626bdc9e
Dylan Warrener <dqyqlqaqn@gmail.com>
2024-03-26 19:29:16 +0000

    init

--- src/components/uncommon/account/profile/uncommon-account-profile.vue
+++ src/components/uncommon/account/profile/uncommon-account-profile.vue
@@ -1,27 +1,63 @@
 <template>
-	<v-card elevation="0" class="h-100" style="border: 2px solid green">
+	<v-card elevation="0" class="h-100 bg-accent" style="border: 2px solid green">
 		<v-container fluid style="border: 2px solid black">
 			<v-row dense style="border: 2px solid red">
 				<v-col cols="12" class="d-flex flex-column justify-center align-center" style="border: 2px solid blue">
 					<!-- Avatar -->
 					<v-hover>
 						<template v-slot:default="{ isHovering, props }">
-							<v-card
-								hover
-								elevation="0"
-								class="rounded-circle"
-								style="position: relative"
-								width="150"
-								height="150"
-								:image="computed_img_canvas"
-								v-bind="props"
-							></v-card>
-							<v-icon id="edit-img" :class="isHovering ? '' : ''" :icon="computed_icon"></v-icon>
+							<div class="h-100" style="position: relative; border: 2px solid red" v-bind="props">
+								<v-card
+									hover
+									class="rounded-circle d-flex justify-center align-center"
+									:style="isHovering ? 'background-color: rgba(0, 0, 0, 0.75)' : ''"
+									width="150"
+									height="150"
+								>
+									<span class="text-inverted text-h3" style="cursor: pointer" v-show="!isHovering">
+										DW
+									</span>
+									<v-expand-transition>
+										<div
+											class="w-100 h-100 d-flex justify-center align-center rounded-circle"
+											style="position: absolute; border: 2px solid red"
+											v-show="isHovering"
+										>
+											<v-tooltip location="bottom" text="Edit picture">
+												<template v-slot:activator="{ props }">
+													<v-btn
+														variant="flat"
+														class="bg-transparent"
+														:icon="computed_icon"
+														v-bind="props"
+														v-show="isHovering"
+													>
+														<template v-slot:default>
+															<v-icon size="32"></v-icon>
+														</template>
+													</v-btn>
+												</template>
+											</v-tooltip>
+										</div>
+									</v-expand-transition>
+								</v-card>
+							</div>
 						</template>
 					</v-hover>
 				</v-col>
+
+				<!-- Profile info -->
+				<v-col cols="12" style="border: 2px solid blue">
+					<v-list>
+						<v-list-subheader>Information</v-list-subheader>
+						<v-list-item></v-list-item>
+					</v-list>
+				</v-col>
 			</v-row>
 		</v-container>
+		<v-card-actions>
+			<v-btn variant="flat" class="text-inverted">Save</v-btn>
+		</v-card-actions>
 	</v-card>
 </template>
 
@@ -36,6 +72,13 @@ import CanvasPNG from "@assets/jpg/temp.jpg";
 
 export default defineComponent({
 	name: "uncommon-account-profile-component",
+	data() {
+		return {
+			profile: {
+				info: {},
+			},
+		};
+	},
 	computed: {
 		computed_icon(): string {
 			return iconsAccountProfile.editImage;
@@ -46,9 +89,3 @@ export default defineComponent({
 	},
 });
 </script>
-
-<style lang="scss">
-#edit-img {
-	position: absolute;
-}
-</style>
