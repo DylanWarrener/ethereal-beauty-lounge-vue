49d8bf4d1e3a2912545d937a80c18164fd37e25b
Dylan Warrener <dqyqlqaqn@gmail.com>
2024-06-17 13:30:27 +0100

    init

--- src/components/common/button/menu/common-button-icon-menu.vue
+++ src/components/common/button/menu/common-button-icon-menu.vue
@@ -1,11 +1,9 @@
-<template>
-    
-</template>
+<template></template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "button-text-menu-container-component"
+	name: "button-text-menu-container-component",
 });
 </script>
