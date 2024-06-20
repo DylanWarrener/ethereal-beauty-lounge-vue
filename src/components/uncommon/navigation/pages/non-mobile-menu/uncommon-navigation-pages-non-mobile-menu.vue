7d875c2f9d2a6b91681611b4734e29dcd72b1bd4
Dylan Warrener <dqyqlqaqn@gmail.com>
2024-06-20 18:55:33 +0100

    init

--- src/components/uncommon/navigation/pages/non-mobile-menu/uncommon-navigation-pages-non-mobile-menu.vue
+++ src/components/uncommon/navigation/pages/non-mobile-menu/uncommon-navigation-pages-non-mobile-menu.vue
@@ -1,42 +1,42 @@
 <template>
 	<common-btn-container-component
 		class="d-none d-md-flex"
-		:id="computed_id_appBar_homeBtn"
-		:text="computed_text_appBar_homeBtn"
-		:to="navigation.home.link"
+		:id="computed_id_appBar_btnHome"
+		:text="computed_text_appBar_btnHome"
+		:to="computed_link_appBar_homeBtn"
 	></common-btn-container-component>
 	<common-btn-container-component
 		class="d-none d-md-flex"
-		:id="computed_id_appBar_treatmentBtn"
-		:text="computed_text_appBar_treatmentBtn"
-		:to="navigation.treatments.link"
+		:id="computed_id_appBar_btnTreatments"
+		:text="computed_text_appBar_btnTreatments"
+		:to="computed_link_appBar_treatmentBtn"
 	></common-btn-container-component>
 	<common-btn-container-component
 		class="d-none d-md-flex"
-		:id="computed_id_appBar_storeBtn"
-		:text="computed_text_appBar_storeBtn"
-		:to="navigation.store.link"
+		:id="computed_id_appBar_btnStore"
+		:text="computed_text_appBar_btnStore"
+		:to="computed_link_appBar_storeBtn"
 	></common-btn-container-component>
 	<common-btn-menu-container-component>
 		<template #menu-btn="{ menuProps, tooltipProps }">
 			<common-btn-container-component
 				class="d-flex d-md-flex"
-				:id="computed_id_appBar_informationBtn"
-				:text="computed_text_appBar_informationBtn"
+				:id="computed_id_appBar_btnInformation"
+				:text="computed_text_appBar_btnInformation"
 				v-bind="mergeProps(menuProps, tooltipProps)"
 				@click="navigation.information.drawer = !navigation.information.drawer"
 			></common-btn-container-component>
 		</template>
 		<template #menu-items>
 			<v-list nav variant="text" bg-color="accent" base-color="white" color="black">
-				<v-list-item class="text-inverted text-capitalize" :to="navigation.information.items.contact.link">
-					<span class="text-default" v-text="navigation.information.items.contact.text"></span>
+				<v-list-item class="text-inverted text-capitalize" :to="computed_link_appBar_contactBtn">
+					<span class="text-default" v-text="computed_text_appBar_btnContact"></span>
 				</v-list-item>
-				<v-list-item class="text-inverted text-capitalize" :to="navigation.information.items.reviews.link">
-					<span class="text-default" v-text="navigation.information.items.reviews.text"></span>
+				<v-list-item class="text-inverted text-capitalize" :to="computed_link_appBar_reviewBtn">
+					<span class="text-default" v-text="computed_text_appBar_btnReviews"></span>
 				</v-list-item>
-				<v-list-item class="text-inverted text-capitalize" :to="navigation.information.items.about.link">
-					<span class="text-default" v-text="navigation.information.items.about.text"></span>
+				<v-list-item class="text-inverted text-capitalize" :to="computed_link_appBar_aboutBtn">
+					<span class="text-default" v-text="computed_text_appBar_btnAbout"></span>
 				</v-list-item>
 			</v-list>
 		</template>
@@ -54,7 +54,7 @@ import BtnMenuContainerComp from "@components/common/menu/common-menu.vue";
 import { btnIDs, txtRouteNames, txtRouteLinks } from "@constants/common/objects/common-constants-objects.js";
 
 // Icons
-import { mdiMenu, mdiMenuUp, mdiMenuDown } from "@constants/common/primitives/icons/common-constants-primative-icons.js";
+import { mdiMenuUp, mdiMenuDown } from "@constants/common/primitives/icons/common-constants-primative-icons.js";
 
 export default defineComponent({
 	name: "navigation-pages-non-mobile-menu-container-component",
@@ -66,38 +66,38 @@ export default defineComponent({
 		return {
 			navigation: {
 				home: {
-					id: btnIDs.appbar.home,
+					id: btnIDs.header.appBar.pcMenu.btn.home.id,
 					text: txtRouteNames.home,
 					link: txtRouteLinks.home,
 				},
 				treatments: {
-					id: btnIDs.appbar.treatments,
+					id: btnIDs.header.appBar.pcMenu.btn.treatments.id,
 					text: txtRouteNames.treatments,
 					link: txtRouteLinks.treatments,
 				},
 				store: {
-					id: btnIDs.appbar.store,
+					id: btnIDs.header.appBar.pcMenu.btn.store.id,
 					text: txtRouteNames.store,
 					link: txtRouteLinks.store,
 				},
 				information: {
 					drawer: false,
-					id: btnIDs.appbar.information,
+					id: btnIDs.header.appBar.pcMenu.btn.information.id,
 					text: "Information",
 					icon: mdiMenuDown,
 					items: {
 						contact: {
-							id: btnIDs.appbar.contact,
+							id: btnIDs.header.appBar.pcMenu.btn.contact.id,
 							text: txtRouteNames.contact,
 							link: txtRouteLinks.contact,
 						},
 						reviews: {
-							id: btnIDs.appbar.reviews,
+							id: btnIDs.header.appBar.pcMenu.btn.reviews.id,
 							text: txtRouteNames.reviews,
 							link: txtRouteLinks.reviews,
 						},
 						about: {
-							id: btnIDs.appbar.about,
+							id: btnIDs.header.appBar.pcMenu.btn.about.id,
 							text: txtRouteNames.about,
 							link: txtRouteLinks.about,
 						},
@@ -107,47 +107,47 @@ export default defineComponent({
 		};
 	},
 	computed: {
-		computed_id_appBar_homeBtn(): string {
+		computed_id_appBar_btnHome(): string {
 			return this.navigation.home.id;
 		},
-		computed_id_appBar_treatmentBtn(): string {
+		computed_id_appBar_btnTreatments(): string {
 			return this.navigation.treatments.id;
 		},
-		computed_id_appBar_storeBtn(): string {
+		computed_id_appBar_btnStore(): string {
 			return this.navigation.store.id;
 		},
-		computed_id_appBar_informationBtn(): string {
+		computed_id_appBar_btnInformation(): string {
 			return this.navigation.information.id;
 		},
-		computed_id_appBar_contactBtn(): string {
+		computed_id_appBar_btnContact(): string {
 			return this.navigation.information.items.contact.id;
 		},
-		computed_id_appBar_reviewBtn(): string {
+		computed_id_appBar_btnReviews(): string {
 			return this.navigation.information.items.reviews.id;
 		},
-		computed_id_appBar_aboutBtn(): string {
+		computed_id_appBar_btnAbout(): string {
 			return this.navigation.information.items.about.id;
 		},
 
-		computed_text_appBar_homeBtn(): string {
+		computed_text_appBar_btnHome(): string {
 			return this.navigation.home.text;
 		},
-		computed_text_appBar_treatmentBtn(): string {
+		computed_text_appBar_btnTreatments(): string {
 			return this.navigation.treatments.text;
 		},
-		computed_text_appBar_storeBtn(): string {
+		computed_text_appBar_btnStore(): string {
 			return this.navigation.store.text;
 		},
-		computed_text_appBar_informationBtn(): string {
+		computed_text_appBar_btnInformation(): string {
 			return this.navigation.information.text;
 		},
-		computed_text_appBar_contactBtn(): string {
+		computed_text_appBar_btnContact(): string {
 			return this.navigation.information.items.contact.text;
 		},
-		computed_text_appBar_reviewBtn(): string {
+		computed_text_appBar_btnReviews(): string {
 			return this.navigation.information.items.reviews.text;
 		},
-		computed_text_appBar_aboutBtn(): string {
+		computed_text_appBar_btnAbout(): string {
 			return this.navigation.information.items.about.text;
 		},
 
